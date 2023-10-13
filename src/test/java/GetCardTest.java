b1a3bc874e1943941b221c4e24c8f138ad0f002f
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2023-10-13 10:51:30 +0300

    fixed

--- src/test/java/GetCardTest.java
+++ src/test/java/GetCardTest.java
@@ -30,8 +30,7 @@ class GetCardTest {
         $("[data-test-id=phone] input").setValue("+79048631049");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
-        $("[data-test-id=name] span.input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -40,8 +39,7 @@ class GetCardTest {
         $("[data-test-id=phone] input").setValue("+79048631049");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
-        $("[data-test-id=name] span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -50,8 +48,7 @@ class GetCardTest {
         $("[data-test-id=phone] input").setValue("+7904863104");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
-        $("[data-test-id=phone] span.input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -60,8 +57,7 @@ class GetCardTest {
         $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
-        $("[data-test-id=phone] span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
